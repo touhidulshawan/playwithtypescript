9126e176ac96808f8f1aab0b595f9f83303fa962
Touhidul Shawan <touhidulshawan@gmail.com>
2021-05-11 23:28:13 +0600

    add function

---
+++ function.ts
@@ -0,0 +1,11 @@
+// * Here function return type is number
+const add = (firstNumber: number, secondNumber: number): number => {
+  const sum = firstNumber + secondNumber;
+  return sum;
+};
+
+// * return type void
+
+const printMessage = (): void => {
+  console.log("Hello Typescript");
+};
